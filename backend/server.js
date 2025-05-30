e36fa4afbcde695f95fe86f0ab5eb79a68b91307
Malavika <malavikaaa2003@gmail.com>
2025-05-30 14:52:29 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -218,12 +218,5 @@ app.post('/api/logout', (req, res) => {
 
 app.get('/', (req, res) => res.send('Cloud Kitchen Backend Running...'));
 
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
