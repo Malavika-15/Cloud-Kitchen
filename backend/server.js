e52c3f727c6cf4c6b03692018d2cdd94ea3cf582
Malavika <malavikaaa2003@gmail.com>
2025-05-30 18:48:38 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['https://your-vercel-domain.vercel.app'],
+  origin: ['https://cloud-kitchen-gules.vercel.app/'],
   credentials: true
 }));
 
