705a35ddf3564c1b2099521605ba4f927eea8e90
Malavika <malavikaaa2003@gmail.com>
2025-05-30 12:20:30 +0530

    third commit

--- Value.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ValueSchema = new mongoose.Schema({
-  icon: String, // e.g., 'Utensils', 'Coffee'
-  title: String,
-  description: String
-});
-
-module.exports = mongoose.model('Value', ValueSchema);
