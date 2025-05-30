b068f31d72473cd421b7a0bafb546c037a76f8d1
Malavika <malavikaaa2003@gmail.com>
2025-05-30 16:45:50 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -210,12 +210,15 @@ app.post('/api/login', async (req, res) => {
     }
 
     const isMatch = await bcrypt.compare(password, admin.password);
+    console.log('Password match:', isMatch);
     if (!isMatch) {
       console.log('Incorrect password');
       return res.status(401).json({ message: 'Invalid credentials' });
     }
 
     const token = jwt.sign({ id: admin._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+    console.log('JWT_SECRET is:', process.env.JWT_SECRET);
+    console.log('Generated token:', token);
 
     res.json({ email: admin.email, token });
   } catch (error) {
